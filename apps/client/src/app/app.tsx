9e66cae33209d813c4054ae8f9f0a97683b306ec
piloshachar14 <piloshachar14@gmail.com>
2024-06-09 15:34:55 +0300

    feature(client):add components to app

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -1,14 +1,27 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
-
-import NxWelcome from './nx-welcome';
-
-export function App() {
+import { Category } from '../components/category/category';
+import { Heading } from '../components/heading/heading';
+import { useState } from 'react';
+export type loginInfo = {
+  isLogIn: boolean;
+};
+export const App: React.FC = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
   return (
-    <div>
-      <NxWelcome title="golden-toasts" />
+    <div className={styles.container}>
+      <Heading isLogin={isLoggedIn} />
+      <div className={styles.categories}>
+        {isLoggedIn ? (
+          <>
+            <Category style={{ width: '33%' }} title="criminals!" />
+            <Category style={{ width: '33%' }} title="toasts:" />
+            <Category style={{ width: '33%' }} title="record" />
+          </>
+        ) : (
+          <Category style={{ width: '100%' }} title="upcoming toasts:" />
+        )}
+      </div>
     </div>
   );
-}
-
+};
 export default App;
