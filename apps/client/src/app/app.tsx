0e58b52271237fe4222d47d2ef38b58a1e024d99
piloshachar14 <155834132+piloshachar14@users.noreply.github.com>
2024-06-10 11:54:55 +0300

    Feature/css changes (#13)
    * feature(client):app css changes
    
    * feature(client):heading change isLoggedIn? and css change
    
    * refactor(client):destructure props
    
    * refactor(client):ternery inside the component

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -3,19 +3,28 @@ import styles from './app.module.css';
 import { Heading, Category } from '../components';
 import { useState } from 'react';
 export const App: React.FC = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <div className={styles.container}>
-      <Heading isLogin={isLoggedIn} />
+      <Heading
+        title={
+          isLoggedIn
+            ? '!נפגשים שוב תומר הפרסונה נון גרטה'
+            : '!ברוך הבא למדור ביצועים'
+        }
+        desc={isLoggedIn ? '' : 'להרשמה  כניסה לחץ על הכפתור מימין'}
+        isLogin={isLoggedIn}
+      ></Heading>
+
       <div className={styles.categories}>
         {isLoggedIn ? (
           <>
-            <Category style={{ width: '33%' }} title="criminals!" />
-            <Category style={{ width: '33%' }} title="toasts:" />
-            <Category style={{ width: '33%' }} title="record" />
+            <Category style={{ width: '33%' }} title=":שיא" />
+            <Category style={{ width: '33%' }} title=":שתיות" />
+            <Category style={{ width: '33%' }} title="!פושעים" />
           </>
         ) : (
-          <Category style={{ width: '100%' }} title="upcoming toasts:" />
+          <Category style={{ width: '100%' }} title=":שתיות קרובות" />
         )}
       </div>
     </div>
