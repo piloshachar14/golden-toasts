48d2ea3373c6fb7cd49d2ef1081bb1534d1d0611
piloshachar14 <piloshachar14@gmail.com>
2024-06-13 09:47:56 +0300

    refactor(client):change of category component to match purpose

--- apps/client/src/components/category/category.tsx
+++ apps/client/src/components/category/category.tsx
@@ -1,17 +1,16 @@
-
 import styles from './category.module.css';
-import React, { CSSProperties } from 'react';
+import React, { PropsWithChildren, CSSProperties } from 'react';
 type Props = {
   style?: CSSProperties;
   title: string;
-};
+} & PropsWithChildren;
 export type CategoryWidth = string;
 
-export const Category: React.FC<Props> = (props) => {
+export const Category: React.FC<Props> = ({ children, style, title }) => {
   return (
-    <div className={styles.category} style={props.style}>
-      <h1 className={styles.title}>{props.title}</h1>
+    <div className={styles.category} style={style}>
+      <h1 className={styles.title}>{title}</h1>
+      {children}
     </div>
   );
-
 };
