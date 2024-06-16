1474c3c91d9690fb273bd412ce3449b85377eee0
piloshachar14 <piloshachar14@gmail.com>
2024-06-16 15:56:56 +0300

    feature(client):add toasts component parameters and styles

--- apps/client/src/components/card/card.tsx
+++ apps/client/src/components/card/card.tsx
@@ -1,23 +1,30 @@
 import { CSSProperties } from 'react';
 import styles from './card.module.css';
+import { style } from '@mui/system';
 type Props = {
   title: string;
   stringBorder?: string;
   desc: string;
-  descStyle: CSSProperties;
+  descStyle?: CSSProperties;
+  toastsDate?: Date;
+  height?: string;
 };
 export const Card: React.FC<Props> = ({
   title,
   desc,
   stringBorder,
   descStyle,
+  toastsDate,
+  height,
 }) => {
+  const style: Record<string, string> = {};
+  if (stringBorder) style['border'] = stringBorder;
+  if (height) style['height'] = height;
+
   return (
-    <div
-      className={styles.card}
-      style={stringBorder ? { border: stringBorder } : {}}
-    >
+    <div className={styles.card} style={style}>
       <div className={styles.heading}>{title}</div>
+      {toastsDate && <div>{toastsDate.toDateString()}</div>}
       <div className={styles.desc} style={descStyle}>
         {desc}
       </div>
