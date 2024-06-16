dc1cde32e9afcc4dd2b0d1a524b30de840265bbe
piloshachar14 <piloshachar14@gmail.com>
2024-06-16 15:55:19 +0300

    feature(client):change category height , add toast component

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -1,5 +1,5 @@
 import styles from './app.module.css';
-import { Heading, Category, Criminals } from '../components';
+import { Heading, Category, Criminals, Toasts } from '../components';
 import { useState } from 'react';
 export const App: React.FC = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(true);
@@ -18,17 +18,21 @@ export const App: React.FC = () => {
       <div className={styles.categories}>
         {isLoggedIn ? (
           <>
-            <Category style={{ width: '33%' }} title=":שיא" />
-            <Category style={{ width: '33%' }} title=":שתיות" />
+            <Category style={{ width: '33%', height: '100%' }} title=":שיא" />
+            <Category style={{ width: '33%', height: '100%' }} title=":שתיות">
+              <Toasts isLoggedIn={true}></Toasts>
+            </Category>
             <Category
-              style={{ width: '33%', overflow: 'scroll' }}
+              style={{ width: '33%', overflow: 'scroll', height: '100%' }}
               title="!פושעים"
             >
               <Criminals></Criminals>
             </Category>
           </>
         ) : (
-          <Category style={{ width: '100%' }} title=":שתיות קרובות" />
+          <Category style={{ width: '100%' }} title=":שתיות קרובות">
+            <Toasts isLoggedIn={false}></Toasts>
+          </Category>
         )}
       </div>
     </div>
