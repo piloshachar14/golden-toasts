cbacec2fe2cbf2f77828909c26c8b97384eea26b
piloshachar14 <piloshachar14@gmail.com>
2024-06-17 14:30:40 +0300

    refactor(client): change imports

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -1,6 +1,5 @@
-import { BrowserRouter } from 'react-router-dom';
-import { Routes, Route } from 'react-router-dom';
-import { MainPage } from '../components/';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { MainPage } from '../components';
 export const App: React.FC = () => {
   return (
     <BrowserRouter>
