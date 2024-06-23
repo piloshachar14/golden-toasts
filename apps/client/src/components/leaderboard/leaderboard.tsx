841c670b2036e44c80928aa2043f16c7f5d9fa03
piloshachar14 <piloshachar14@gmail.com>
2024-06-23 17:17:51 +0300

    feature(client): add leaderboard component

--- apps/client/src/components/leaderboard/leaderboard.tsx
+++ apps/client/src/components/leaderboard/leaderboard.tsx
@@ -1,7 +1,10 @@
+import styles from './leaderboard.module.css';
 import { useState } from 'react';
-import { Category, User } from '../';
+import { Category, Divider, User } from '../';
 import { LeaderboardRounded } from '@mui/icons-material';
 import { Tooltip } from 'react-tooltip';
+import Badge from '@mui/material/Badge';
+import Avatar from '@mui/material/Avatar';
 export const Leaderboard: React.FC = () => {
   const [leaderboard, setLeaderboard] = useState<User[]>([]);
   return (
@@ -17,6 +20,44 @@ export const Leaderboard: React.FC = () => {
       <Tooltip anchorSelect=".leaderboard" place="top">
         :טבלת מובילים
       </Tooltip>
+      <div className={styles.leaderboardTable}>
+        <div className={styles.leaderBoardOne}>
+          <Avatar
+            sx={{
+              bgcolor: 'gold',
+              top: '25%',
+              left: '1em',
+            }}
+          >
+            N
+          </Avatar>
+          <Divider style={{ height: '100%', width: '0.1em' }} />
+        </div>
+        <div className={styles.leaderBoardOne}>
+          <Avatar
+            sx={{
+              bgcolor: 'silver',
+              top: '25%',
+              left: '1em',
+            }}
+          >
+            N
+          </Avatar>
+          <Divider style={{ height: '100%', width: '0.1em' }} />
+        </div>
+        <div className={styles.leaderBoardOne}>
+          <Avatar
+            sx={{
+              bgcolor: '#cd7f32',
+              top: '25%',
+              left: '1em',
+            }}
+          >
+            N
+          </Avatar>
+          <Divider style={{ height: '100%', width: '0.1em' }} />
+        </div>
+      </div>
     </Category>
   );
 };
