2cec2f2e16c8324fa21578cca0b282f17a5f53d3
piloshachar14 <piloshachar14@gmail.com>
2024-06-27 18:25:48 +0300

    feature(client): change leaderboard icon ->react-icons and color bluer

--- apps/client/src/components/leaderboard/leaderboard.tsx
+++ apps/client/src/components/leaderboard/leaderboard.tsx
@@ -1,43 +1,44 @@
 import styles from './leaderboard.module.css';
 import { useState } from 'react';
 import { Category, LeaderboardCard, User } from '../';
-import { LeaderboardRounded } from '@mui/icons-material';
+import { MdLeaderboard } from 'react-icons/md';
 import { Tooltip } from 'react-tooltip';
+import { IconContext } from 'react-icons';
 
 export const Leaderboard: React.FC = () => {
   const [leaderboard, setLeaderboard] = useState<User[]>([]);
   return (
     <Category style={{ border: 'none', height: '50%' }}>
-      <div>
-        <LeaderboardRounded
-          className="leaderboard"
-          sx={{
-            color: 'blue',
-          }}
-        />
-      </div>
-      <Tooltip anchorSelect=".leaderboard" place="top">
-        טבלת מובילים
-      </Tooltip>
-      <div className={styles.leaderboardTable}>
-        <LeaderboardCard
-          avatarColor="gold"
-          leaderboard={leaderboard}
-          className="goldBorderColor"
-          dividerClassName="dividerGold"
-        />
-        <LeaderboardCard
-          avatarColor="silver"
-          leaderboard={leaderboard}
-          className="silverBorderColor"
-          dividerClassName="dividerSilver"
-        />
-        <LeaderboardCard
-          avatarColor="#cd7f32"
-          leaderboard={leaderboard}
-          className="bornzeBorderColor"
-          dividerClassName="dividerBronze"
-        />
+      <div className={styles.leaderboard}>
+        <IconContext.Provider value={{ size: '1.4em' }}>
+          <div className={styles.button}>
+            <MdLeaderboard className={`${styles.leaderboard} leaderboard`} />
+            <Tooltip anchorSelect=".leaderboard" place="top">
+              טבלת מובילים
+            </Tooltip>
+          </div>
+        </IconContext.Provider>
+
+        <div className={styles.leaderboardTable}>
+          <LeaderboardCard
+            avatarColor="gold"
+            leaderboard={leaderboard}
+            className="goldBorderColor"
+            dividerClassName="dividerGold"
+          />
+          <LeaderboardCard
+            avatarColor="silver"
+            leaderboard={leaderboard}
+            className="silverBorderColor"
+            dividerClassName="dividerSilver"
+          />
+          <LeaderboardCard
+            avatarColor="#cd7f32"
+            leaderboard={leaderboard}
+            className="bornzeBorderColor"
+            dividerClassName="dividerBronze"
+          />
+        </div>
       </div>
     </Category>
   );
