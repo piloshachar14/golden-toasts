facc56f6facbed74639b0e49ced3d5cf43511b5d
piloshachar14 <piloshachar14@gmail.com>
2024-07-02 13:29:56 +0300

    feature(client): change category to fit record state component

--- apps/client/src/components/category/category.tsx
+++ apps/client/src/components/category/category.tsx
@@ -1,16 +1,36 @@
+import { RecordState } from '../record-state';
 import styles from './category.module.css';
 import React, { PropsWithChildren, CSSProperties } from 'react';
 type Props = {
   style?: CSSProperties;
   title?: string;
+  toastsbutton?: boolean;
 } & PropsWithChildren;
 export type CategoryWidth = string;
 
-export const Category: React.FC<Props> = ({ children, style, title }) => {
+export const Category: React.FC<Props> = ({
+  children,
+  style,
+  title,
+  toastsbutton,
+}) => {
   return (
-    <div className={styles.category} style={style}>
-      {title && <h1 className={styles.title}>{title}</h1>}
-      <div className={styles.children}>{children}</div>
-    </div>
+    <>
+      <div className={styles.category} style={style}>
+        {toastsbutton ? (
+          <div className={styles.toasts}>
+            <div className={styles.recordstate}>
+              <RecordState />
+            </div>
+            {title && <h1 className={styles.title}>{title}</h1>}
+          </div>
+        ) : (
+          <div className={styles.title}>
+            {title && <h1 className={styles.title}>{title}</h1>}
+          </div>
+        )}
+        <div className={styles.children}>{children}</div>
+      </div>
+    </>
   );
 };
