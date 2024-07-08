4bbbadeb4fc831aa8338e408c11daeada3fc1787
piloshachar14 <155834132+piloshachar14@users.noreply.github.com>
2024-07-08 13:39:01 +0300

    Feature/sign in/up dialog (#27)
    * refactor(client): chnage imports and leaderboard css to the leaderboard component
    
    * feature(client): add sign in component
    
    * refactor(client): remove unnessary app.module.css
    
    * feature(client): add sign in state button and component
    
    * refactor(client): change imports,names and delete unnessary color

--- apps/client/src/components/heading/heading.tsx
+++ apps/client/src/components/heading/heading.tsx
@@ -1,57 +1,68 @@
 import styles from './heading.module.css';
 import LoginIcon from '@mui/icons-material/Login';
 import SettingsIcon from '@mui/icons-material/Settings';
+import { useState } from 'react';
+import { SignIn } from '../sign-in-form';
 type Props = {
   isLogin: boolean;
   title: string;
   desc: string;
 };
 export const Heading: React.FC<Props> = ({ title, desc, isLogin }) => {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
   const BackgroundColor = '#4d4855';
+  const handleOnButtonClick = () => {
+    setIsDialogOpen(true);
+  };
+
   return (
-    <div className={styles.heading}>
-      <div className={styles.title}>
-        <h1>{title}</h1>
-        <h2>{desc}</h2>
+    <>
+      <div className={styles.heading}>
+        <div className={styles.title}>
+          <h1>{title}</h1>
+          <h2>{desc}</h2>
+        </div>
+        {!isLogin ? (
+          <LoginIcon
+            sx={{
+              position: 'sticky',
+              color: '#80FFFF',
+              backgroungcolor: BackgroundColor,
+              right: 40,
+              top: 40,
+              fontSize: 90,
+              '&:hover': {
+                cursor: 'pointer',
+                color: '#40FFFF',
+                transition: 'transform 0.3s ease-in-out',
+                transform: 'scale(1.2)',
+              },
+            }}
+            onClick={() => handleOnButtonClick()}
+          />
+        ) : (
+          <SettingsIcon
+            sx={{
+              position: 'sticky',
+              color: '#80FFFF',
+              backgroungcolor: BackgroundColor,
+              right: 40,
+              top: 20,
+              fontSize: 90,
+              transition: 'transform 0.3s ease-in-out',
+              '&:hover': {
+                transitionDelay: 1,
+                cursor: 'pointer',
+                color: '#008080',
+                transition: 'transform 0.3s ease-in-out',
+                transform: 'rotate(90deg)',
+              },
+            }}
+          />
+        )}
+        ;
       </div>
-      {!isLogin ? (
-        <LoginIcon
-          sx={{
-            position: 'sticky',
-            color: '#80FFFF',
-            backgroungcolor: BackgroundColor,
-            right: 40,
-            top: 40,
-            fontSize: 70,
-            '&:hover': {
-              cursor: 'pointer',
-              color: '#40FFFF',
-              transition: 'transform 0.3s ease-in-out',
-              transform: 'scale(1.2)',
-            },
-          }}
-        />
-      ) : (
-        <SettingsIcon
-          sx={{
-            position: 'sticky',
-            color: '#80FFFF',
-            backgroungcolor: BackgroundColor,
-            right: 40,
-            top: 20,
-            fontSize: 90,
-            transition: 'transform 0.3s ease-in-out',
-            '&:hover': {
-              transitionDelay: 1,
-              cursor: 'pointer',
-              color: '#008080',
-              transition: 'transform 0.3s ease-in-out',
-              transform: 'rotate(90deg)',
-            },
-          }}
-        />
-      )}
-      ;
-    </div>
+      <SignIn isDialogOpen={isDialogOpen} setIsDialogOpen={setIsDialogOpen} />
+    </>
   );
 };
--- apps/client/src/store/index.ts
+++ apps/client/src/store/index.ts
@@ -1,2 +1,3 @@
 export * from './store';
 export * from './services';
+export * from './types';
