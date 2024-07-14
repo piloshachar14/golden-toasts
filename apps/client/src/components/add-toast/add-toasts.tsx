c3f300d0588b3af4c3010ad70dbbfb68dcdc0529
piloshachar14 <piloshachar14@gmail.com>
2024-07-14 14:15:05 +0300

    feature(client): create add-toast dialog

--- apps/client/src/components/add-toast/add-toasts.tsx
+++ apps/client/src/components/add-toast/add-toasts.tsx
@@ -7,7 +7,19 @@ import {
   TextField,
   DialogTitle,
   createTheme,
+  DialogContent,
+  InputLabel,
+  Select,
+  SelectChangeEvent,
+  MenuItem,
+  FormControl,
+  OutlinedInput,
 } from '@mui/material';
+
+import { LocalizationProvider, DatePicker } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+
 import { useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
 
@@ -25,6 +37,9 @@ export const AddToast: React.FC<Props> = ({
       mode: 'dark',
     },
   });
+  const handleOnClose = () => {
+    setIsAddToastDialogOpe(false);
+  };
   const dialogStyle = {
     direction: 'rtl',
     width: '100%',
@@ -36,17 +51,150 @@ export const AddToast: React.FC<Props> = ({
       width: '37.5rem',
     },
   };
+  const dialogContentStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'column',
+    gap: '1rem',
+    color: 'var(--white-color)',
+    '&.MuiDialogContent-root': {
+      height: '80%',
+    },
+  };
+
+  const buttonStyles = {
+    fontSize: '1rem',
+    width: '90%',
+    color: 'black',
+    transitionDuration: '100ms',
+    '&:hover': {
+      cursor: 'pointer',
+      transform: 'scale(1.2)',
+    },
+  };
 
   const handleClickAway = () => {
     setIsAddToastDialogOpe(false);
   };
 
+  const [solidsPick, setSolidsPick] = useState<string[]>([]);
+
+  const handleChange = (event: SelectChangeEvent<typeof solidsPick>) => {
+    const {
+      target: { value },
+    } = event;
+    setSolidsPick(typeof value === 'string' ? value.split(',') : value);
+  };
+
+  const solids = [
+    'פלאפל',
+    'פיצות',
+    '',
+    'גלידה',
+    'קרקרים ומטבלים',
+    'פירות',
+    'עוגיות',
+    'עוגות',
+  ];
+
+  const fluids = ['אייס קפה', 'שוקו', '', 'מיץ', 'סודה', 'מים בטעמים', 'קולה'];
+
   return (
     <ThemeProvider theme={darkTheme}>
-      <ClickAwayListener onClickAway={handleClickAway}>
-        <Dialog open={isAddToastDialogOpen} sx={dialogStyle}></Dialog>
-      </ClickAwayListener>
-      ;
+      <Dialog
+        open={isAddToastDialogOpen}
+        sx={dialogStyle}
+        onClose={handleOnClose}
+      >
+        <DialogTitle
+          sx={{
+            padding: '2rem',
+          }}
+          component="h1"
+          align="center"
+        >
+          הוסיפו שתייה לקראת האירוע הבא שלכם!
+        </DialogTitle>
+
+        <DialogContent sx={dialogContentStyle}>
+          <Stack
+            component="form"
+            sx={{
+              gap: '2rem',
+              padding: '1rem',
+              width: '80%',
+            }}
+          >
+            <TextField
+              placeholder="אנא ציין את סיבת השתייה"
+              name="description"
+              required
+            />
+            <FormControl sx={{ width: '100%', direction: 'rtl' }}>
+              <InputLabel id="solids-label">בחר את השתייה</InputLabel>
+              <Select
+                labelId="demo-multiple-name-label"
+                id="demo-multiple-name"
+                multiple
+                value={solidsPick}
+                onChange={handleChange}
+                input={<OutlinedInput label="Name" />}
+              >
+                {fluids.map((fluid) => (
+                  <MenuItem key={fluid} value={fluid}>
+                    {fluid}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+            <FormControl sx={{ width: '100%', direction: 'rtl' }}>
+              <InputLabel id="solids-label">בחר את האוכל</InputLabel>
+              <Select
+                labelId="demo-multiple-name-label"
+                id="demo-multiple-name"
+                multiple
+                value={solidsPick}
+                onChange={handleChange}
+                input={<OutlinedInput label="Name" />}
+              >
+                {solids.map((solid) => (
+                  <MenuItem key={solid} value={solid}>
+                    {solid}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DemoContainer components={['DatePicker']}>
+                <DatePicker
+                  sx={{ direction: 'rtl', width: '100%' }}
+                  label="בחרו תאריך לשתייה"
+                />
+              </DemoContainer>
+            </LocalizationProvider>
+          </Stack>
+
+          <Stack gap="4rem" direction="row">
+            <Button
+              sx={buttonStyles}
+              className="cancel"
+              variant="contained"
+              onClick={() => handleClickAway()}
+            >
+              ביטול
+            </Button>
+            <Button
+              sx={buttonStyles}
+              className="submit"
+              type="submit"
+              variant="contained"
+              onClick={() => handleClickAway()}
+            >
+              הוספה
+            </Button>
+          </Stack>
+        </DialogContent>
+      </Dialog>
     </ThemeProvider>
   );
 };
