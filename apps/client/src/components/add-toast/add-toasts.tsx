c62b53e6ccaf0e1950b4882405062e42297a72d5
piloshachar14 <piloshachar14@gmail.com>
2024-07-18 16:26:46 +0300

    ..

--- apps/client/src/components/add-toast/add-toasts.tsx
+++ apps/client/src/components/add-toast/add-toasts.tsx
@@ -19,11 +19,9 @@ import rtlPlugin from 'stylis-plugin-rtl';
 import { CacheProvider } from '@emotion/react';
 import createCache from '@emotion/cache';
 import { prefixer } from 'stylis';
-
 import { LocalizationProvider, DatePicker } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
-
 import { useState } from 'react';
 
 type Props = {
