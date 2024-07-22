74472313e382fef13d3202e957a74916a197b842
piloshachar14 <piloshachar14@gmail.com>
2024-07-22 13:38:25 +0300

    feature(client): add criminalApi to store

--- apps/client/src/store/store.ts
+++ apps/client/src/store/store.ts
@@ -1,11 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { userApi, toastApi } from './services';
+import { userApi, toastApi, criminalApi } from './services';
 
 export const store = configureStore({
   reducer: {
     [userApi.reducerPath]: userApi.reducer,
     [toastApi.reducerPath]: toastApi.reducer,
+    [criminalApi.reducerPath]: criminalApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(userApi.middleware, toastApi.middleware),
+    getDefaultMiddleware().concat(
+      userApi.middleware,
+      toastApi.middleware,
+      criminalApi.middleware
+    ),
 });
