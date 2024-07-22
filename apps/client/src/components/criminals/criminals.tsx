05f02d5f54e9dffe93e3e32d896e669e13e73283
piloshachar14 <piloshachar14@gmail.com>
2024-07-22 13:36:50 +0300

    feature(client): change criminals and criminals api to get all criminals and filter it

--- apps/client/src/components/criminals/criminals.tsx
+++ apps/client/src/components/criminals/criminals.tsx
@@ -1,17 +1,18 @@
-import { useState } from 'react';
 import styles from './criminals.module.css';
 import { Card, Category, Divider } from '../';
 import { Tooltip } from 'react-tooltip';
 import { GiPirateFlag, GiPirateGrave } from 'react-icons/gi';
 import { IconContext } from 'react-icons';
-import {
-  useGetAllNonPersonaNonGratasQuery,
-  useGetAllPersonaNonGratasQuery,
-} from '../../store';
+import { Criminal, useGetAllCriminalsQuery } from '../../store';
 
 export const Criminals: React.FC = () => {
-  const { data: CriminalsData } = useGetAllNonPersonaNonGratasQuery();
-  const { data: PersonaNonGratasData } = useGetAllPersonaNonGratasQuery();
+  const { data: allCriminals } = useGetAllCriminalsQuery();
+  const CriminalsData = allCriminals?.filter(
+    (criminal: Criminal) => !criminal.isPersonaNonGrata
+  );
+  const PersonaNonGratasData = allCriminals?.filter(
+    (criminal: Criminal) => criminal.isPersonaNonGrata
+  );
   return (
     <div className={styles.criminals}>
       <Divider />
@@ -33,6 +34,8 @@ export const Criminals: React.FC = () => {
             {CriminalsData?.map((criminal, index) => (
               <Card
                 key={index}
+                title={criminal.user.fullName}
+                description="תאריך שהפך לפושע"
                 toastsDate={criminal.createdAt}
                 stringBorder="0.1rem var(---red-border-color) solid"
                 descriptionStyle={{
@@ -65,6 +68,8 @@ export const Criminals: React.FC = () => {
             {PersonaNonGratasData?.map((pesonaNonGrata, index) => (
               <Card
                 key={index}
+                title={pesonaNonGrata.user.fullName}
+                description="תאריך שהפך לפושע"
                 toastsDate={pesonaNonGrata.createdAt}
                 stringBorder="0.1rem white solid"
                 descriptionStyle={{
--- apps/client/src/store/services/criminalsApi.ts
+++ apps/client/src/store/services/criminalsApi.ts
@@ -5,15 +5,9 @@ export const criminalApi = createApi({
   reducerPath: 'criminalApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000/api' }),
   endpoints: (builder) => ({
-    getAllPersonaNonGratas: builder.query<Criminal[], void>({
-      query: () => '/AllPersonaNonGratas',
-    }),
-    getAllNonPersonaNonGratas: builder.query<Criminal[], void>({
-      query: () => 'AllNonPersonaNonGratas',
+    getAllCriminals: builder.query<Criminal[], void>({
+      query: () => '/criminals',
     }),
   }),
 });
-export const {
-  useGetAllNonPersonaNonGratasQuery,
-  useGetAllPersonaNonGratasQuery,
-} = criminalApi;
+export const { useGetAllCriminalsQuery } = criminalApi;
