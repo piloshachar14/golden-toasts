5391c42d797f5b8a7e3285b094d5cea148e88490
piloshachar14 <piloshachar14@gmail.com>
2024-07-29 13:38:34 +0300

    refactor(client): move all cards related component to a folder and add a new card.tsx component

--- apps/client/src/components/card/card.tsx
+++ apps/client/src/components/card/card.tsx
@@ -1,19 +1,8 @@
-import styles from './card.module.css';
-import { PropsWithChildren } from 'react';
-import { CardDate, CardDescription, CardHeader } from '..';
+import { CardDate, CardDescription, CardHeader, CardRoot } from '.';
 
-type Props = {
-  stringBorder?: string;
-} & PropsWithChildren;
-export const Card: React.FC<Props> = ({ stringBorder, children }) => {
-  const style: Record<string, string> = {};
-  if (stringBorder) {
-    style['border'] = stringBorder;
-  }
-
-  return (
-    <div className={styles.card} style={style}>
-      {children}
-    </div>
-  );
+export const Card = {
+  Root: CardRoot,
+  Header: CardHeader,
+  Description: CardDescription,
+  Date: CardDate,
 };
