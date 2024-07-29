ae0ea15d2e2d96e0946c6d789814a900a27feb90
piloshachar14 <piloshachar14@gmail.com>
2024-07-29 12:08:09 +0300

    refactor(client): add new subcomponent card-date

---
+++ apps/client/src/components/card-date/card-date.tsx
@@ -0,0 +1,14 @@
+import styles from './card-date.module.css';
+
+type CardDateProps = {
+  date: Date;
+};
+
+export const CardDate: React.FC<CardDateProps> = ({ date }) => {
+  const charOfNewDate = 10;
+  const formattedDate = date
+    ? date.toString().substring(0, charOfNewDate)
+    : null;
+
+  return <div className={styles.date}>{formattedDate}</div>;
+};
